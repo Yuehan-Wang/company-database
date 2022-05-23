a2de24aee7edbb3147f97f1be95e4ab42d006952
Yuehan Wang <yuehan@uchicago.edu>
2022-05-23 02:05:42 -0500

    made some changes to search page

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,10 +1,39 @@
 import { useEffect, useState } from "react";
-import { Search } from 'antd';
+import { Button } from "../../shared/button/Button";
+import { Checkbox } from 'antd';
 
 export default function Search() {
+    const ColoredLine = ({ color, height }) => (
+        <hr
+            style={{
+                color: color,
+                backgroundColor: color,
+                height: height,
+                width: window.Width * 4/5,
+                margin: 5
+            }}
+        />
+    );
     return (
-        <div>
-            <p>This is the search page</p>
+        <div className="absolute left-20 w-4/5 text-xl text-lime-800 justify-center items-center font-sans">
+            <div className="text-[#226A32] mt-1 font-black text-5xl">
+                Corporation/LLC Search
+            </div>
+            <ColoredLine color="#226A32" height={5} />
+            <div className="text-[#226A32] mt-1 font-black text-2xl">
+                Type of Search
+            </div>
+            
+        <div className="flex flex-col mt-3 text-base m-1 text-[#226A32]">
+            <Checkbox>Corporate and LLC</Checkbox>
+            <Checkbox>Corporate</Checkbox>
+            <Checkbox>LLC</Checkbox>
+        </div>
+        <ColoredLine color="grey" height={1}/>
+        <div className="absolute left-0 mt-3">
+        <Button />
+        </div>
+
         </div>
     )
 }
--- src/shared/button/Button.jsx
+++ src/shared/button/Button.jsx
@@ -8,7 +8,7 @@ export function Button() {
         <div className="w-fit h-fit rounded-full border bg-[#226A32] border-[#226A32] hover:bg-white active:!bg-[#226A32] active:shadow-inner active:border-[#226A32] transition-all group">
             <button 
                 className="font-sans text-base text-white group-hover:text-primary-600 active:text-primary-600 transition-all py-[10px] px-[10px]"
-                onClick={() => {navigate('/result')}}
+                onClick={() => {navigate('/search')}}
             >
                 Search
             </button>
